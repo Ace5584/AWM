c2d93d52c4a7e17d066c1ce0f0d8a1f9d62ecf20
Alex <aexace666@gmail.com>
2022-01-23 15:28:52 +1100

    AfterClassification Controller if statement logics completed

--- Trashify/AfterClassificationViewController.swift
+++ Trashify/AfterClassificationViewController.swift
@@ -10,11 +10,40 @@ import UIKit
 class AfterClassificationViewController: UIViewController {
 
     var index: Int?
+    var binColor: String!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        print(index)
-        // Do any additional setup after loading the view.
+        let dataPhoto = UserDefaults.standard.object(forKey: "ClassifyPicture") as! [NSData]
+        let dataLabel = UserDefaults.standard.object(forKey: "ClassifyResult") as! [String]
+        let splitLabel = dataLabel[index ?? 0].components(separatedBy: "|")
+        
+        if splitLabel[1] == "battery"{
+            binColor = "Special"
+        }
+        else if splitLabel[1] == "biological"{
+            binColor = "Green"
+        }
+        else if splitLabel[1] == "cardboard"{
+            binColor = "Yellow"
+        }
+        else if splitLabel[1] == "clothes"{
+            binColor = "Red"
+        }
+        else if splitLabel[1] == "glass"{
+            binColor = "Red"
+        }
+        else if splitLabel[1] == "metal"{
+            binColor = "Yellow"
+        }
+        else if splitLabel[1] == "paper"{
+            binColor = "Yellow"
+        }
+        else if splitLabel[1] == "paper"{
+            binColor = "Yellow"
+        }
+        
+        print(binColor + " - " + splitLabel[1])
     }
     
 }
