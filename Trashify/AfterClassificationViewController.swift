c8f965a8c15e8d2d8bf2314cabfd528041b632a6
Alex <aexace666@gmail.com>
2022-01-24 13:43:40 +1100

    Added UIImage that shows what color trash can the classified trash should go in to

--- Trashify/AfterClassificationViewController.swift
+++ Trashify/AfterClassificationViewController.swift
@@ -9,6 +9,7 @@ import UIKit
 
 class AfterClassificationViewController: UIViewController {
 
+    @IBOutlet weak var trashIcon: UIImageView!
     var index: Int?
     var binColor: String!
     
@@ -44,6 +45,16 @@ class AfterClassificationViewController: UIViewController {
         }
         
         print(binColor + " - " + splitLabel[1])
+        
+        if binColor == "Green"{
+            trashIcon.image = UIImage(named: "GreenTrashCan")
+        }
+        else if binColor == "Red"{
+            trashIcon.image = UIImage(named: "RedTrashCan")
+        }
+        else if binColor == "Yellow"{
+            trashIcon.image = UIImage(named: "YellowTrashCan")
+        }
     }
     
 }
