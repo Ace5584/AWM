5fbc168c63b8226f8a9aceafd8c8110688b83588
Alex <aexace666@gmail.com>
2022-04-18 21:11:48 +1000

    Added color bin to history details page

--- Trashify/Trashify/HistoryDetailsViewController.swift
+++ Trashify/Trashify/HistoryDetailsViewController.swift
@@ -9,9 +9,11 @@ import UIKit
 
 class HistoryDetailsViewController: UIViewController {
     
+    @IBOutlet weak var binColorPhoto: UIImageView!
     @IBOutlet weak var photo: UIImageView!
     @IBOutlet weak var label: UILabel!
     var index: Int?
+    var binColor: String!
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -19,6 +21,19 @@ class HistoryDetailsViewController: UIViewController {
         label.text = dataLabel[index ?? 0]
         let dataPhoto = UserDefaults.standard.object(forKey: "ClassifyPicture") as! [NSData]
         photo.image = UIImage(data: dataPhoto[index ?? 0] as Data)
+        
+        let splitLabel = dataLabel[index ?? 0].components(separatedBy: "|")
+        binColor = splitLabel[3]
+        
+        if binColor == "Green"{
+            binColorPhoto.image = UIImage(named: "GreenTrashCan")
+        }
+        else if binColor == "Red"{
+            binColorPhoto.image = UIImage(named: "RedTrashCan")
+        }
+        else if binColor == "Yellow"{
+            binColorPhoto.image = UIImage(named: "YellowTrashCan")
+        }
 
         // Do any additional setup after loading the view.
     }
